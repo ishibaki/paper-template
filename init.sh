9e8e815d33af5f4a1da58231119bd28d408e94c5
ishibaki <iroirotomoki@gmail.com>
2019-07-13 21:39:06 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ chmod +x ./pre-push
 chmod +x ./bin/logger.sh
 chmod +x ./bin/make_docx.sh
 chmod +x ./bin/make_pdf.sh
+chmod +x ./bin/log_analyzer.py
 
 NOW_DATE=`date "+%F"`
 sed -i -e "s/YYYY-MM-DD/${NOW_DATE}/" ./README.md
