ff8d9e38274c70aa3d9dde20a97f359c47968c60
ishibaki <iroirotomoki@gmail.com>
2019-07-13 17:27:27 +0900

    Update logger.sh

--- bin/logger.sh
+++ bin/logger.sh
@@ -7,4 +7,4 @@ NOW_WORD=`expr $NOW_WORD - 113`
 NOW_LETR=`cat ./manuscript/*.md | wc -m | tr -d ' '`
 NOW_LETR=`expr $NOW_LETR - 1373`
 
-echo $NOW_DATE,$NOW_TIME,$NOW_WORD,$NOW_LETR >> .log/writing_log.csv
+echo $NOW_DATE,$NOW_TIME,$NOW_WORD,$NOW_LETR >> ./log/writing_log.csv
