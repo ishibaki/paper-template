a0295af4fe744d1062f7c36e018850e2796b6ca8
ishibaki <iroirotomoki@gmail.com>
2019-07-13 17:28:39 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sed -e "s/YYYY-MM-DD/${NOW_DATE} ./README.md" >| ./README.md
 git init
 mv pre-push ./.git/hooks/pre-push
 mv post-commit ./.git/hooks/post-commit
-.bin/logger.sh
+./bin/logger.sh
 mkdir figs
 mkdir out
 mkdir release
