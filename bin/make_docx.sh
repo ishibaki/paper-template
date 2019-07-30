0072dca9cfa0d3b3d9f8418773985f2d3b0a26fe
ishibaki <iroirotomoki@gmail.com>
2019-07-30 11:49:49 +0900

    Update make_docx.sh

--- bin/make_docx.sh
+++ bin/make_docx.sh
@@ -5,8 +5,8 @@ pandoc -f markdown -t docx -V papersize=a4\
     --reference-doc ~/.pandoc/reference.docx\
     --filter pandoc-docx-pagebreakpy\
     --filter pandoc-crossref\
-    --bibliography="/Users/tishibashi/dotfiles/pandoc_files/library.bib"\
-    --csl="/Users/tishibashi/dotfiles/pandoc_files/apa-2.csl"\
+    --bibliography="${HOME}/.pandoc/library.bib"\
+    --csl="${HOME}/.pandoc/apa-2.csl"\
     -V papersize=a4 -N -o ../out/`date "+%y%m%d"`_out.docx\
     ./0_metadata/*.md ./*.md
 
