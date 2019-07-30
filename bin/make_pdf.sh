03fb3e45fb866be4287c345ed1eb7b4185d17600
ishibaki <iroirotomoki@gmail.com>
2019-07-30 11:50:30 +0900

    Update make_pdf.sh

--- bin/make_pdf.sh
+++ bin/make_pdf.sh
@@ -3,8 +3,8 @@
 cd manuscript
 pandoc -f markdown -t latex -V papersize=a4\
     --filter pandoc-crossref --template=eisvogel\
-    --bibliography="/Users/tishibashi/dotfiles/pandoc_files/library.bib"\
-    --csl="/Users/tishibashi/dotfiles/pandoc_files/apa-2.csl"\
+    --bibliography="${HOME}/.pandoc/library.bib"\
+    --csl="${HOME}/.pandoc/apa-2.csl"\
     -V papersize=a4 --pdf-engine=lualatex --toc -N\
     -o ../out/`date "+%y%m%d"`_out.pdf\
     ./0_metadata/*.md ./*.md
