a58da48652ad2d42a02e16d2d8e4a1e9f413c910
ishibaki <iroirotomoki@gmail.com>
2023-03-15 16:46:15 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-chmod +x ./post-commit
-chmod +x ./pre-push
-chmod +x ./bin/logger.sh
-chmod +x ./bin/make_docx.sh
-chmod +x ./bin/make_pdf.sh
-chmod +x ./bin/log_analyzer.py
-
 echo "Enter the title:"
 read title
-sed -i -e "s/^\# Papaer Template/# ${title}/" ./README.md
+sed -i -e "s/^\# Papar Template/# ${title}/" ./README.md
 NOW_DATE=`date "+%F"`
 sed -i -e "s/YYYY-MM-DD/${NOW_DATE}/" ./README.md
+
 git init
-mv pre-push ./.git/hooks/pre-push
-mv post-commit ./.git/hooks/post-commit
-./bin/logger.sh
+
 mkdir figs
 mkdir out
 mkdir release
